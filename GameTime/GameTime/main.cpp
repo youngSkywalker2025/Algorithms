937ebd1895153d62ea78df765814ba3d0baf67a6
ArjunTrushenPatel <arjun_ragu@outlook.com>
2024-09-23 22:11:43 -0500

    Initial Commit

---
+++ GameTime/GameTime/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GameTime
+//
+//  Created by Arjun Trushen Patel on 9/23/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
